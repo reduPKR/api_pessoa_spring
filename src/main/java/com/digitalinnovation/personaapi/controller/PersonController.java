d70f018a46c30d695efe35a04833a38677a82c77
redu <reduardo19942@gmail.com>
2020-12-10 00:16:26 -0300

    projeto criado

---
+++ src/main/java/com/digitalinnovation/personaapi/controller/PersonController.java
@@ -0,0 +1,14 @@
+package com.digitalinnovation.personaapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+    @GetMapping
+    public String getPerson(){
+        return "Harry Potter";
+    }
+}
